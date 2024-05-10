96235549e2008c2f5e53c92513aecc4e2fdd30dd
Kim yeongjun <cosmoyj7733@gmail.com>
2024-05-10 16:37:05 +0900

    classification file and folder

---
+++ client/src/object/colorType.js
@@ -0,0 +1,18 @@
+export const UI_color = {
+  main_color : {
+    prime : "#D9D9D9",
+    signIn : "#D1D1D1",
+    addPost : "#D1D1D1",
+    white : "#fffff",
+    blueButton : "#1150F2",
+    newDot : "#8A0C0C"
+  }
+}
+
+export const text_color = {
+  main_color : {
+    prime : "#000000",
+    white : "#fffff",
+    footer : "#605C5C",
+  }
+}
